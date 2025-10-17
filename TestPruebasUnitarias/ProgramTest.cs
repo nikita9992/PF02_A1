982ddd691b81caa0942b77d991694d63db9da0b0
RBC <raqblacor2@alu.edu.gva.es>
2025-10-17 19:00:03 +0200

    Creado test no finalizado

---
+++ TestPruebasUnitarias/ProgramTest.cs
@@ -0,0 +1,13 @@
+﻿namespace TestPruebasUnitarias
+{
+    [TestClass]
+    public sealed class ProgramTest
+    {
+        [TestMethod]
+        public void esMenorQueCero()
+        {
+            bool resulto = Program.CalcularFactorial(-1);
+            Assert.IsTrue(resulto);
+        }
+    }
+}
