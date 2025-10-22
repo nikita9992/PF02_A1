673ff30169c5dcfb35d0cb9bd87fd3676a3b9b64
Mykyta Vavulin <nik.vavulin@gmail.com>
2025-10-22 20:14:56 +0200

    Test validar contraseña

---
+++ TestPruebasUnitarias2/Test1.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestPruebasUnitarias2
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            [TestMethod]
+            public void EsContrasenyaValida_InputEsNull_DebeDevolverFalse()
+            {
+                Assert.IsFalse(_validador.EsContrasenyaValida(null));
+            }
+
+            [TestMethod]
+            public void EsContrasenyaValida_InputEstaVacio_DebeDevolverFalse()
+            {
+                Assert.IsFalse(_validador.EsContrasenyaValida(""));
+            }
+
+            [TestMethod]
+            public void EsContrasenyaValida_LongitudMenorA8_DebeDevolverFalse()
+            {
+                Assert.IsFalse(_validador.EsContrasenyaValida("pass#12")); // 7 chars
+            }
+
+            [TestMethod]
+            public void EsContrasenyaValida_NoContieneHashtag_DebeDevolverFalse()
+            {
+                Assert.IsFalse(_validador.EsContrasenyaValida("Password12345")); // Larga, sin #
+            }
+
+            [TestMethod]
+            public void EsContrasenyaValida_ContrasenyaValida_DebeDevolverTrue()
+            {
+                Assert.IsTrue(_validador.EsContrasenyaValida("MiPasswordValida#1"));
+            }
+        }
+    }
+}
