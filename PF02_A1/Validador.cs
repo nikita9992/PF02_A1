b388c02db811cd1c8834a41a0aa8157bb9c5889c
RBC <raqblacor2@alu.edu.gva.es>
2025-10-22 20:32:17 +0200

    Validador creado y test pasados

---
+++ PF02_A1/Validador.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PF02_A1
+{
+    public class Validador
+    {
+        public static bool EsContrasenyaValida(string contrasenya)
+        {
+            return !string.IsNullOrEmpty(contrasenya)
+            && contrasenya.Length >= 8
+            && contrasenya.Contains('#');
+        }
+
+    }
+}
--- TestPruebasUnitarias2/ValidadorTest.cs
+++ TestPruebasUnitarias2/ValidadorTest.cs
@@ -1,42 +1,43 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿//using System.ComponentModel.DataAnnotations;
+using PF02_A1;
 
 namespace TestPruebasUnitarias2
 {
     [TestClass]
-    public sealed class Test1
+    public sealed class ValidadorTest
     {
-        [TestMethod]
-        public void TestMethod1()
-        {
+        //[TestMethod]
+        //public void TestMethod1()
+        //{
             [TestMethod]
             public void EsContrasenyaValida_InputEsNull_DebeDevolverFalse()
             {
-                Assert.IsFalse(_validador.EsContrasenyaValida(null));
+                Assert.IsFalse(Validador.EsContrasenyaValida(null));
             }
 
             [TestMethod]
             public void EsContrasenyaValida_InputEstaVacio_DebeDevolverFalse()
             {
-                Assert.IsFalse(_validador.EsContrasenyaValida(""));
+                Assert.IsFalse(Validador.EsContrasenyaValida(""));
             }
 
             [TestMethod]
             public void EsContrasenyaValida_LongitudMenorA8_DebeDevolverFalse()
             {
-                Assert.IsFalse(_validador.EsContrasenyaValida("pass#12")); // 7 chars
+                Assert.IsFalse(Validador.EsContrasenyaValida("pass#12")); // 7 chars
             }
 
             [TestMethod]
             public void EsContrasenyaValida_NoContieneHashtag_DebeDevolverFalse()
             {
-                Assert.IsFalse(_validador.EsContrasenyaValida("Password12345")); // Larga, sin #
+                Assert.IsFalse(Validador.EsContrasenyaValida("Password12345")); // Larga, sin #
             }
 
             [TestMethod]
             public void EsContrasenyaValida_ContrasenyaValida_DebeDevolverTrue()
             {
-                Assert.IsTrue(_validador.EsContrasenyaValida("MiPasswordValida#1"));
+                Assert.IsTrue(Validador.EsContrasenyaValida("MiPasswordValida#1"));
             }
         }
     }
-}
+//}
